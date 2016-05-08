d1a5d1faf4da8a9f0888cd35348b21159d4c4579
sgbj <scott.batary@gmail.com>
2016-05-08 16:17:13 -0500

    Mobile working

--- dist/sidebar.js
+++ dist/sidebar.js
@@ -1,6 +1,6 @@
 var slickbar;
 (function (slickbar) {
-    function showSidebar(element) {
+    function showSidebar(element, test) {
         if (!element) return;
         if (!element.classList.contains('active')) {
             element.classList.add('active');
@@ -26,13 +26,22 @@ var slickbar;
     function onShowSidebar(e) {
         e.preventDefault();
         var selector = this.getAttribute('data-sidebar-show');
-        showSidebar(selector ? document.querySelector(selector) : this);
+        showSidebar(selector ? document.querySelector(selector) : this, e.touches);
     }
 
     function onHideSidebar(e) {
         e.preventDefault();
         var selector = this.getAttribute('data-sidebar-hide');
-        hideSidebar(selector ? document.querySelector(selector) : this);
+        var sidebar = document.querySelector(selector);
+        console.log('hiding ', sidebar, selector, this)
+        if ((sidebar && sidebar.classList.contains('active')) || this.classList.contains('active')) {
+            hideSidebar(selector ? document.querySelector(selector) : this);
+        } else {
+            var triggers = this.querySelectorAll('[data-sidebar-show]');
+            for (var i = 0; i < triggers.length; i++) {
+                hideSidebar(document.querySelector(triggers[i].getAttribute('data-sidebar-show')));
+            }
+        }
     }
 
     function onToggleSidebar(e) {
@@ -51,11 +60,17 @@ var slickbar;
             for (var i = 0; i < sidebarHiders.length; i++) {
                 sidebarHiders[i].addEventListener('click', onHideSidebar.bind(sidebarHiders[i]));
             }
+            
+            var parentSidebars = document.querySelectorAll('.sidebar:not(.child)');
+            for (var i = 0; i < parentSidebars.length; i++) {
+                //parentSidebars[i].addEventListener('touchstart', onShowSidebar.bind(parentSidebars[i]));
+            }
 
             var sidebars = document.querySelectorAll('.sidebar');
             for (var i = 0; i < sidebars.length; i++) {
                 sidebars[i].addEventListener('mouseenter', onShowSidebar.bind(sidebars[i]));
                 sidebars[i].addEventListener('mouseleave', onHideSidebar.bind(sidebars[i]));
+                //sidebars[i].addEventListener('touchend', onHideSidebar.bind(sidebars[i]));
             }
 
             var sidebarTogglers = document.querySelectorAll('[data-sidebar-toggle]');
